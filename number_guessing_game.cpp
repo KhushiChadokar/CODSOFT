4f5fc7299ec068ec711b291c451aa0efc12c7012
KhushiChadokar <162139378+KhushiChadokar@users.noreply.github.com>
2024-04-15 22:20:34 +0530

    Add files via upload

---
+++ number_guessing_game.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+using namespace std;
+int main()
+{
+    int random_number,guess,attempts=0;
+    srand((unsigned int) time(NULL));
+    random_number = rand() % 100 + 1;
+
+//applying do-while loop to enter the number(guess) until the correct number has been guessed by the player
+    cout << "Hey player!! guess the number between 1 and 100: ";
+    do{
+
+        cin >> guess;
+        if (guess < random_number)
+        {
+            cout << "Your guess is too low! Try again: ";
+        }
+        else if(guess > random_number)
+        {
+            cout << "Your guess is too high! Try again: ";
+        }
+        else{
+            cout<<"Congratulations you guessed it right with total attempts of : "<< attempts <<endl;
+        }
+        attempts++;
+        
+    }
+    
+    while (guess!= random_number);
+    
+    return 0;
+}
